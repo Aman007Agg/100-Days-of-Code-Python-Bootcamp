ccca50941612f6a379df535104f610ec962db00a
Aman007Agg <G24ait172@iitj.ac.in>
2026-05-06 11:10:57 +0530

    output functions- Calculator Programme

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,59 @@
+from art import logo
+print(logo)
 def add(n1, n2):
     return n1 + n2
+
+# my_favourite_calculation = add #You can store a reference to a function as a value to a variable. e.g.
+# print(my_favourite_calculation(3, 5))
+# print(type(my_favourite_calculation(3, 5)))
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+" : add,
+    "-" : subtract,
+    "*" : multiply,
+    "/" : divide,
+}
+
+# TODO: Use the dictionary operations to perform the calculations. Multiply 4 * 8 using the dictionary.
+# print(operations[*](4,8))  #it will give 32
+
+def calculator():
+    print(logo)
+    should_proceed = True
+    n1 = int(input("Enter the first number:"))
+
+    while should_proceed:
+        # prev_result = 0
+        for symbol in operations:
+            print(symbol)
+        choice = input('Enter the type of mathematical operation - a choice of "+", "-", "*" or "/" ')
+        n2 = int(input("Enter the Second number:"))
+        # ops = operations[choice]
+        # prev_result += ops(n1, n2)
+        # print(prev_result)
+        answer = operations[choice](n1, n2)
+        print(f"{n1} {choice} {n2} = {answer}")
+        wants_to_continue = input(f"wants to continue calculating with {answer} : type 'yes' or if not then type 'no' :").lower()
+        if wants_to_continue == "yes":
+            n1 = answer
+        elif wants_to_continue == "no":
+            should_proceed = False
+            calculator()
+        else:
+            print("Invalid input, Kindly enter the valid response")
+            wants_to_continue = input("wants to continue : type 'yes' or if not then type 'no' :").lower()
+
+
+
+calculator()
 
 
--- Day 10/Dcostrings/task.py
+++ Day 10/Dcostrings/task.py
@@ -1,12 +1,29 @@
+from operator import truediv
+
+
 def format_name(f_name, l_name):
+    """ take a first and last name and format
+    it to  return the title case version of
+     the name"""
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
     return f"{formated_f_name} {formated_l_name}"
 
 
-formatted_name = format_name("AnGeLa", "YU")
-
+formatted_name = format_name("AMAN", "agrawal")
+print(formatted_name)
 length = len(formatted_name)
 
 
+def is_leap_year(year):
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
+        return False
+    elif year % 4 == 0:
+        return True
+    else:
+        return False
 
+output = is_leap_year(2024)
+print(output)
