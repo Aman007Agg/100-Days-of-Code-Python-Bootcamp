b089d9762e6f0c41b0f9b597e976d32615a2428b
Aman007Agg <G24ait172@iitj.ac.in>
2026-05-08 17:05:09 +0530

    Completed Day 13 debugging exercises

--- Day 13/Use a Debugger/task.py
+++ Day 13/Use a Debugger/task.py
@@ -9,8 +9,18 @@ def mutate(a_list):
         new_item = item * 2
         new_item += random.randint(1, 3)
         new_item = maths.add(new_item, item)
-    b_list.append(new_item)
+        b_list.append(new_item)
     print(b_list)
 
 
 mutate([1, 2, 3, 5, 8, 13])
+
+
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number"
+
+
+print(odd_or_even(12))
