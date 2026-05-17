9672a7335d9d7310b4136f54277eece1339ed3f3
Aman007Agg <G24ait172@iitj.ac.in>
2026-05-17 08:08:04 +0530

    Snake Game - project using class - Inheritance concepts

--- Day 20/main.py
+++ Day 20/main.py
@@ -1,6 +1,8 @@
 from tracemalloc import Snapshot
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -12,6 +14,16 @@ screen.tracer(0)
 """
 snake = Snake()
 # Note: We have mode moved the snake code into a separate class
+
+"""
+class inheritance concepts- creating food
+"""
+food = Food()
+
+"""
+class to track scoreboard
+"""
+scoreboard = Scoreboard()
 
 """
 3. how to control the snake with keypress
@@ -31,9 +43,35 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    """
+        4. snake collision detection with food using distance method.
+    """
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        # 5. Create a scoreboard class to hold the scoreboard
+        scoreboard.increase_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        game_is_on = False
+        scoreboard.game_over()
 
+    #Detect collision with tail
+    # if head collides with any segment in the tail:
+        #trigger game over
+    # the below concepts can be made easier with slicing concept
 
+    # for segment in snake.segments:
+    #     if segment == snake.head:
+    #         pass
+    #     elif snake.head.distance(segment) < 10:
+    #         game_is_on = False
+    #         scoreboard.game_over()
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
